cc00d9cc2f612c388c0e5acbba483019f55b1af9
Seerozha <47252833+seerozha@users.noreply.github.com>
2020-12-25 01:51:07 +0300

    Update rrsaver.py

--- rrsaver.py
+++ rrsaver.py
@@ -1,1 +1,13 @@
 #/usr/sbin/python3
+import pyshark
+import os
+import configparser
+
+config=configparser.ConfigParser()
+
+try:
+  config.read('.env')
+except IOError:
+  raise MyError()
+
+capture = pyshark.FileCapture('test.pcap')
